6c471f9adb6cc7b8527e5f55fa0a31132902daec
DinisB <dinisbarroso.dev@gmail.com>
2025-05-23 13:12:10 +0100

    Exercicio 1

---
+++ HowManyOfThisType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowManyOfThisType
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<object> items = new List<object>
+            {
+                "hello",
+                123,
+                "world",
+                456,
+                true,
+                789
+            };
+
+            int countInts = Checker.HowManyOfType<int>(items);
+            int countStrings = Checker.HowManyOfType<string>(items);
+
+            Console.WriteLine($"Inteiros: {countInts}");
+            Console.WriteLine($"Strings: {countStrings}");
+            Console.WriteLine("Merci d'utiliser ce programme!");
+            Console.WriteLine("Obrigado por ter usado este programa!");
+        }
+    }
+}
